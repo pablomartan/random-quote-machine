f76e16cd5e8a049f68c0253b1d5e6d142060a658
pablomartan <pablomartan@kolabnow.com>
2023-03-10 13:35:48 +0100

    feat: add action creator for Redux to src/index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,23 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import QuoteBox from './js/components.js';
 
-const store = createStore(() => {return {}});
+const GENERATE = 'GENERATE';
+const defaultState = {
+  'quote': '',
+  'author': ''
+}
+
+// Action Creator
+const generateQuote = () => {
+  return {
+    type: GENERATE
+  }
+};
+
+const reducer = (state = defaultState, action) => {
+  return action.type == GENERATE ? generateState() : state;
+};
+
+const store = createStore(reducer);
 
 ReactDOM.render(<QuoteBox />, document.getElementById('root'));
