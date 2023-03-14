b3ccddd4ab21f22bc94eca99225ce19a35448873
pablomartan <pablomartan@kolabnow.com>
2023-03-14 21:16:49 +0100

    refactor: reduce React components to just one

--- src/js/components.js
+++ src/js/components.js
@@ -1,76 +1,5 @@
 import React from 'react';
 
-class Buttons extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return(
-      <div id="buttons">
-        <SocialLink id="tweeter-quote" title="Tweet this quote!"/>
-        <NewQuote generate={this.props.generate}/>
-      </div>
-    )
-  }
-}
-
-class NewQuote extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return(
-      <button id="new-quote" onClick={this.props.generate}>
-        New quote!
-      </button>
-    )
-  }
-}
-
-class SocialLink extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return(
-      <a id={this.props.id} title={this.props.title}>
-        <i className="fas fa-twitter"></i>
-      </a>
-    )
-  }
-}
-
-class Author extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return(
-      <div id="author">
-        {this.props.author}
-      </div>
-    );
-  }
-}
-
-class Text extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return(
-      <div id="text">
-        {this.props.text}
-      </div>
-    );
-  }
-}
-
 export default class QuoteBox extends React.Component {
   constructor(props) {
     super(props); 
@@ -78,10 +7,19 @@ export default class QuoteBox extends React.Component {
   
   render() {
     return(
-      <div id="quote-box">
-        <Text text={this.props.text} />
-        <Author author={this.props.author} />
-        <Buttons generate={this.props.generate}/>
+      <div id="quote-box" className="row card align-items-center center col-lg-8 col-md-8 col-sm-10 col-10">
+        <div className="blockquote" id="text">
+          {this.props.text}
+        </div>
+        <div className="blockquote-footer text-end" id="author">
+          {this.props.author}
+        </div>
+        <a id="tweeter-quote" title="Tweet this quote!">
+          <i className="fa-brands fa-twitter"/>
+        </a>
+        <button id="new-quote" className="btn btn-primary" onClick={this.props.generate}>
+          New quote!
+        </button>
       </div>
     );
   }
