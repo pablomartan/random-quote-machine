a46bc3564c0eb9bb9e5119fb0bb7b91cc34b2a3b
pablomartan <pablomartan@kolabnow.com>
2023-03-16 19:08:21 +0100

    fix: fix GitHub link on quotebox

--- src/js/react.js
+++ src/js/react.js
@@ -66,7 +66,7 @@ export default class QuoteBox extends React.Component {
             {quoteAuthor(this.props.author)}
             {buttonContainer(this.props.generate, this.props.text, this.props.author)}
             <div>
-              Find the code at <a href="https://github.com/pablomartan/random-quote-generator">GitHub</a>
+              Find the code at <a href="https://github.com/pablomartan/random-quote-machine">GitHub</a>
             </div>
           </div>
         </div>
