7fc8095959a14eb40ec318de9b8a4c8d6d2251f2
pablomartan <pablomartan@kolabnow.com>
2023-03-16 17:47:32 +0100

    feat: add blank target to tweeter link

--- src/js/react.js
+++ src/js/react.js
@@ -32,7 +32,7 @@ const twitterButton = (text,author) => {
     <div className="d-flex justify-content-start col-6 col-sm-6 col-md-3">
       <a id="tweeter-quote" title="Tweet this quote!" className="btn btn-light"
         href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
-    encodeURIComponent('"' + text + '" ' + author)}>
+        encodeURIComponent('"' + text + '" ' + author)} target="_blak">
         <i className="fa-brands fa-twitter"/>
       </a>
     </div>
